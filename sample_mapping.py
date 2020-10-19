abc0bb484866e04196523d8ec6cc8f2df7e2ca35
Artur Gottmann <artur.gottmann@kit.edu>
2020-10-19 18:51:15 +0200

    sample_mapping.py: update to Phase2

--- sample_mapping.py
+++ sample_mapping.py
@@ -1,29 +1,18 @@
-# runtype_to_sample = {
-#     'QCD'  : 'RelValQCD_FlatPt_15_3000HS_14',
-#     'TTbar': 'RelValTTbar_14TeV',
-#     'ZEE'  : 'RelValZEE_14',
-#     'ZMM'  : 'RelValZMM_14', 
-# }
 runtype_to_sample = {
-    'QCD'  : 'RelValQCD_FlatPt_15_3000HS_13',
-    'TTbar': 'RelValTTbar_13',
-    'ZEE'  : 'RelValZEE_13',
-    'ZMM'  : 'RelValZMM_13', 
-    'ZTT'  : 'RelValZTT_13', 
+    'QCD'  : 'RelValQCD_Pt15To7000_Flat_14TeV',
+    'TTbar': 'RelValTTbar_14TeV',
+    'ZEE'  : 'RelValZEE_14',
+    'ZMM'  : 'RelValZMM_14', 
+    'ZTT'  : 'RelValZTT_14TeV', 
+    'ZpTT'  : 'RelValZpTT_1500_14TeV', 
+    'TenTaus'  : 'RelValTenTau_15_500', 
 }
-# runtype_to_sample = {
-#     'DYToLL' : 'RelValDYToLL_M_50_14TeV',
-#     'ZTT': 'RelValZTT_14TeV',
-#     #'ZTT': 'RelValZTT_13',
-#     'ZMM': 'RelValZMM_13',
-#     'ZpMM': 'RelValZpMM_13',
-#     'ZEE': 'RelValZEE_13',
-#     # 'QCD': 'RelValQCD_FlatPt_15_3000HS_13',
-#     'QCD' : "RelValQCD_Pt-15To7000_Flat_14TeV",
-#     # 'TTbar': 'RelValTTbar_13',
-#     'TTbar': 'RelValTTbar_14TeV',
-#     'TTbarTau': 'RelValTTbar_13',
-#     'ZpTT': 'RelValZpTT_1500_13',
-#     'TenTaus': 'RelValTenTau_15_500',
-# }
+
+# /RelValQCD_Pt15To7000_Flat_14TeV/CMSSW_11_1_2_patch1-111X_mcRun4_realistic_T15_v1_HLTTDRPU200MINIv2-v1/GEN-SIM-DIGI-RAW-MINIAOD
+# /RelValTTbar_14TeV/CMSSW_11_1_2_patch1-111X_mcRun4_realistic_T15_v1_HLTTDRPU200MINIv2-v1/GEN-SIM-DIGI-RAW-MINIAOD
+# /RelValTenTau_15_500/CMSSW_11_1_2_patch1-111X_mcRun4_realistic_T15_v1_HLTTDRPU200MINIv2-v1/GEN-SIM-DIGI-RAW-MINIAOD
+# /RelValZEE_14/CMSSW_11_1_2_patch1-111X_mcRun4_realistic_T15_v1_HLTTDRPU200MINIv2-v1/GEN-SIM-DIGI-RAW-MINIAOD
+# /RelValZMM_14/CMSSW_11_1_2_patch1-111X_mcRun4_realistic_T15_v1_HLTTDRPU200MINIv2-v1/GEN-SIM-DIGI-RAW-MINIAOD
+# /RelValZTT_14TeV/CMSSW_11_1_2_patch1-111X_mcRun4_realistic_T15_v1_HLTTDRPU200MINIv2-v1/GEN-SIM-DIGI-RAW-MINIAOD
+# /RelValZpTT_1500_14TeV/CMSSW_11_1_2_patch1-111X_mcRun4_realistic_T15_v1_HLTTDRPU200MINIv2-v1/GEN-SIM-DIGI-RAW-MINIAOD
 
